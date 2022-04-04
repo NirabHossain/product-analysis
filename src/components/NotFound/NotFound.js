3dd9e309d8211521de7b733f7ad51c6031b4472a
NirabHossain <nirabamth@gmail.com>
2022-04-04 15:24:56 +0600

    redirects

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            Sorry, the page you are asking for is not found
+        <div className='text-center'>
+            <h1 className='mt-5 bg-warning'>
+                Error 404
+            </h1>
+            <p className='text-danger'>
+                Sorry, the page you are asking for is not found. Please try again with the valid address.
+            </p>
         </div>
     );
 };
